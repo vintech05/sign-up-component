844f6f37b84a6c4e8b435bbc01e7da68b7899d59
vintech05 <kotakkelpo@gmail.com>
2023-07-09 16:52:48 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+const form = document.querySelector('form');
+const submitBtn = document.querySelector('trial')
+const password = document.querySelector('password');
+
+    form.addEventListener('submit', (e) => {
+
+        const inputs = document.querySelectorAll('input');
+  
+        inputs.forEach(input => {
+          input.nextElementSibling.innerHTML = '';
+          const val = input.value.trim();
+  
+          if (val === '') {
+            setError(input, `this field cannot be empty`);
+            input.style.outline = '2px solid var(--Red)';
+            input.style.color = 'var(--Red)';
+  
+          } else if (input.id === 'email' && !isEmail(val)) {
+            setError(input, 'Looks like this is not an email');
+            input.style.outline = '2px solid var(--Red)';
+            input.style.color = 'var(--Red)';
+          } 
+
+          e.preventDefault();
+          e.target.blur();
+          
+        });
+    })
+    
+    submitBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      e.target.blur();
+    });
+
+    function setError(input, message) {
+      input.nextElementSibling.insertAdjacentHTML('beforeend', errorMarkup(message));
+    }
+
+    function errorMarkup(message) {
+      return `
+        <div class="error-text">${message}</div>`;
+    }
+
+    function isEmail(email) {
+      const pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return pattern.test(String(email).toLowerCase());
+    }
---
+++ style-guide.md
@@ -0,0 +1,32 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+- 
+### Accent
+
+- 
+
+### Neutral
+
+- 
+
+## Typography
+
+### Body Copy
+
+- Font size: 16px
+
+### Font
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 400, 500, 600, 700
